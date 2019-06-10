c4ed810b2002d80eceb174d0d578bc02e4528f09
ZeyuKeithFu <zeyufu@bu.edu>
2019-06-09 22:14:42 -0400

    Add vapor.

---
+++ vapor/js/vapor.js
@@ -0,0 +1,40 @@
+// playlist object
+var playList = {
+    0: new Howl({
+            src: ['sounds/demo.mp3']
+        }),
+    1: new Howl({
+            src: ['sounds/demo.mp3']
+        }),
+    2: new Howl({
+            src: ['sounds/demo.mp3']
+        }),
+    length: 3,
+};
+
+// Play function
+$(".song").on("click", function (e) {
+    var pos = $(this).index();
+    var toPlay = playList[pos];
+    stopSongs();
+    $($(".fa-compact-disc")[pos]).toggleClass("playing");
+    $($(".fa-stop-circle")[pos]).toggleClass("playing");
+    toPlay.play();
+    e.stopPropagation();
+});
+
+// Stop playing function
+$(".fa-stop-circle").on("click", function (e) {
+    stopSongs();
+    e.stopPropagation();
+});
+
+function stopSongs() {
+    for (var i = 0; i < playList.length; i++) {
+        if (playList[i].playing()) {
+            playList[i].stop();
+            $($(".fa-compact-disc")[i]).toggleClass("playing");
+            $($(".fa-stop-circle")[i]).toggleClass("playing");
+        }
+    }
+}
